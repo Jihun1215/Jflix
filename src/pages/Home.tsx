56917ced68bbb1e12a674b0c35268efb3192851d
음지훈 <jhmc5268@naver.com>
2023-11-22 14:38:52 +0900

    Feat: 데이터 패칭 함수 추가 및 필요없는 코드 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,25 +7,33 @@ import { Banner, Slider, Spinner } from 'components';
 
 export const Home = () => {
   const type = 'movie';
-  const { data: popularMovieList, isLoading: loadingMovie } = useQuery(['popularMovie', type, 'popular'], () =>
-    getContents(type, 'popular')
-  );
+  const {
+    data: popularMovieList,
+    isLoading: popularLoading,
+    isError: popularError,
+  } = useQuery(['popularMovie', type, 'popular'], () => getContents(type, 'popular'));
 
-  const { data: topRatedMovieList, isLoading: nowPlayLoading } = useQuery(['getTopReatedMovies', type, 'top_rated'], () =>
-    getContents(type, 'top_rated')
-  );
+  const {
+    data: topRatedMovieList,
+    isLoading: TopReatedLoading,
+    isLoading: TopReatedError,
+  } = useQuery(['getTopReatedMovie', type, 'top_rated'], () => getContents(type, 'top_rated'));
 
-  const { data: upcomingMovieList, isLoading: upcomingMovieLoading } = useQuery(['getTopReatedMovie', type, 'upcoming'], () =>
-    getContents(type, 'upcoming')
-  );
+  const {
+    data: upcomingMovieList,
+    isLoading: upcomingLoading,
+    isLoading: upcomingError,
+  } = useQuery(['getUpcomginMovie', type, 'upcoming'], () => getContents(type, 'upcoming'));
 
-  // const { data: nowPlayMovieList, isLoading: nowPlayMovieLoading } = useQuery(['getNowPlatMovie', type, 'now-playing'], () =>
-  //   getContents(type, 'now-playing')
-  // );
+  const {
+    data: nowPlayMovieList,
+    isLoading: nowPlayLoading,
+    isLoading: nowPlayError,
+  } = useQuery(['getNowPlatMovie', type, 'now_playing'], () => getContents(type, 'now_playing'));
 
-  console.log(topRatedMovieList, upcomingMovieList);
 
-  const isLoading = loadingMovie || nowPlayLoading || upcomingMovieLoading;
+  const isLoading = popularLoading || TopReatedLoading || upcomingLoading || nowPlayLoading;
+  const isError = popularError || TopReatedError || upcomingError || nowPlayError;
 
   if (isLoading) {
     return (
@@ -35,16 +43,16 @@ export const Home = () => {
     );
   }
 
-  // if (isError) {
-  //   return <div>Error fetching data</div>;
-  // }
+  if (isError) {
+    return <div>Error fetching data</div>;
+  }
 
   const randomNumber = Math.floor(Math.random() * 5);
 
   return (
     <Container>
       <Banner type="movie" contents={popularMovieList?.results?.[randomNumber]} ranking={randomNumber} />
-      {/* <Slider type="movie" contents={nowPlauyMovieList?.results} title="인기 영화" /> */}
+      <Slider type="movie" contents={nowPlayMovieList?.results} title="개봉중인 영회" />
       <Slider type="movie" contents={topRatedMovieList?.results} title="최고평점 영화" />
       <Slider type="movie" contents={upcomingMovieList?.results} title="개봉예정 영화" />
     </Container>
--- src/pages/Tv.tsx
+++ src/pages/Tv.tsx
@@ -2,28 +2,39 @@ import styled from 'styled-components';
 import { useQuery } from 'react-query';
 
 import { getContents } from 'utils/api';
-import { Banner, Slider } from 'components';
+import { Banner, Slider, Spinner } from 'components';
 
 export const Tv = () => {
   const type = 'tv';
-  const { data: popularLists, isLoading: popularLoading } = useQuery(['getpopilarTv', type, 'popular'], () => getContents(type, 'popular'));
+  const {
+    data: popularLists,
+    isLoading: popularLoading,
+    isError: popularError,
+  } = useQuery(['getpopilarTv', type, 'popular'], () => getContents(type, 'popular'));
 
-  const { data: airingTodayLists, isLoading: airingTodayLoading } = useQuery(['getairingTodayTv', type, 'airing_today'], () =>
-    getContents(type, 'airing_today')
-  );
+  const {
+    data: airingTodayLists,
+    isLoading: airingTodayLoading,
+    isError: airingTodayTvError,
+  } = useQuery(['getairingTodayTv', type, 'airing_today'], () => getContents(type, 'airing_today'));
 
-  const { data: topRatedTvData, isLoading: topRatedTvLoading } = useQuery(['topRatedTv', type, 'top_rated'], () =>
-    getContents(type, 'top_rated')
-  );
+  const {
+    data: topRatedTvData,
+    isLoading: topRatedTvLoading,
+    isError: topRetedTvError,
+  } = useQuery(['topRatedTv', type, 'top_rated'], () => getContents(type, 'top_rated'));
 
   const isLoading = popularLoading || airingTodayLoading || topRatedTvLoading;
+  const isError = popularError || airingTodayTvError || topRetedTvError;
 
   if (isLoading) {
-    return <div>1isLoading</div>;
+    return <Spinner />;
   }
-  // if (isError) {
-  //   return <div>1isError</div>;
-  // }
+
+  if (isError) {
+    return <div>1isError</div>;
+  }
+  
   const randomNumber = Math.floor(Math.random() * 5);
   return (
     <Container>
